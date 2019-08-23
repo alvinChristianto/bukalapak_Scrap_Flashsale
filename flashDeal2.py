a11b875df07d2298dc326914d98bd8cee608793e
alvinChristianto <alvinchristianto93@gmail.com>
2019-08-23 22:29:07 +0700

    add comment on flashDeal2.py

--- flashDeal2.py
+++ flashDeal2.py
@@ -11,11 +11,14 @@ from mysql.connector import errorcode
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-#get time for flashsale
+
 def initial():
+    #get html source
     with open("docu/flashdeal.html") as fp:
+        #convert html file to bs4 object
         soup = BeautifulSoup(fp, "html5lib")
         
+        #get html tree tag 
         header = soup.find("body", {"class", "reskinned_page"})
         header1 = header.find("div", {"class", "c-outer-wrapper c-outer-wrapper--split flash_deal-c index-a js-layout layout"})
         #header2 = header1.find("div", {"class", "jupiter"})
@@ -27,14 +30,17 @@ def initial():
         headerli = header6.find("li", {"class", "c-tab__list c-tab__list--inside o-layout__item u-width-2of10 u-mrgn--0 is-active"})
         headerA = headerli.find("a", {"class", "c-tab__link u-align-center u-pad-bottom--2"})
         headerTime = headerA.contents[2]
+        #get Time of flashdeal
         print headerTime.text
  
      
 #get flash sale item 
 def initial2():
+    #convert html file to bs4 obj 
     with open("docu/flashdeal.html") as fp:
         soup = BeautifulSoup(fp, "html5lib")
         
+        #get html tag tree
         header = soup.find("body", {"class", "reskinned_page"})
         header1 = header.find("div", {"class", "c-outer-wrapper c-outer-wrapper--split flash_deal-c index-a js-layout layout"})
         #header2 = header1.find("div", {"class", "jupiter"})
@@ -50,6 +56,7 @@ def initial2():
       
         
         print("\r\r\r")
+        #looping on every item
         i = 0 
         while True:
             try:
@@ -68,22 +75,27 @@ def initial2():
                 header15a = header15a.replace(" Tersisa", "")
                 header13a = header13.find("span", {"class", "c-product-price__original u-mrgn-right--0"})
                 header13b = header13.find("span", {"class", "c-product-price__reduced u-fg--red-brand"})
+                #get item name
                 print('item %d = %s'%(i,header12.text))
+
+                #get original price (before discount)
                 print('harga original  '+header13a.text)
+                
+                #get discount price
                 print('harga reduced  '+header13b.text)
+                
+                #get the remain item
                 print('sisa  '+header15a) 
+                
                 i += 1
             except(IndexError):
+                #when there is no item left to scrap
                 break
             print("\n\n\n")
        
        
      
-       
-       
-
-
-
+#main
 if __name__ == "__main__":
     print(initial())
     print(initial2())
